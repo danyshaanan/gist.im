f5fe512742d47608f75d58bbadf26cd8200da5b0
Dany <danyshaanan@gmail.com>
2014-07-30 01:16:33 +0300

    serve the page only on valid gist ids

--- main.js
+++ main.js
@@ -3,11 +3,15 @@
 var config = require('./config') //either js: 'module.exports = {}' or json: '{}'
 var express = require('express')
 
+var validGistIdPathRegex = /^\/(\d+|[\da-f]{20})$/
+
 var app = express()
 app.use(express.static(__dirname + '/public/'))
-app.get(/^(.*)$/, function(req, res, next) {
+app.get(validGistIdPathRegex, function(req, res, next) {
   return res.sendfile(__dirname + '/public/query.htm')
 })
+
+// Add error on invalid paths
 
 app.listen(config.port)
 console.log('now listening to port', config.port)
