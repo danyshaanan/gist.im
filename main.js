231b6983a526c8496110e1a4b585bc0910de780b
Dany <danyshaanan@gmail.com>
2014-07-29 20:05:27 +0300

    removed uneeded options and cleaned code

--- main.js
+++ main.js
@@ -5,15 +5,7 @@ var GitHubApi = require('github')
 var marked = require('marked')
 var express = require('express')
 
-var github = new GitHubApi({
-  version: '3.0.0',
-  // optional
-  // debug: true,
-  protocol: 'https',
-  // pathPrefix: '/api/v3', // for some GHEs
-  timeout: 5000
-})
-
+var github = new GitHubApi({ version: '3.0.0', debug: config.debug })
 github.authenticate({ type: 'oauth', token: config.token })
 
 var app = express()
@@ -32,12 +24,12 @@ app.get(/^(.*)$/, function(req, res, next) {
     var html = mdFiles.map(function(file){return marked(file.content)}).join('<hr />')
     res.end('<span id="source" style="display:none">' + html + '</span><script>copy("#source","#content")</script>')
   })
-});
+})
 
 app.use(function(err, req, res, next){
-  console.error(err.stack);
+  console.error(err.stack)
   res.status(500).send('Something broke!')
-});
+})
 
 app.listen(config.port)
 console.log('now listening to port', config.port)
