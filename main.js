abe020305ecec88fa6199d9a34caa18f2849bf11
Dany <danyshaanan@gmail.com>
2014-07-29 20:40:13 +0300

    extracted getGist and renderGist

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 'use strict'
 
 var config = require('./config') //either js: 'module.exports = {}' or json: '{}'
-var GitHubApi = require('github')
+var getGist = require('./lib/getGist.js')(config)
 var marked = require('marked')
 var express = require('express')
 
-var github = new GitHubApi({ version: '3.0.0', debug: config.debug })
-github.authenticate({ type: 'oauth', token: config.token })
+var validGistIdRegex = /^(\d+|[\da-f]{20})$/
 
-var app = express()
-
-app.use(express.static(__dirname + '/public/'))
-
-app.get(/^(.*)$/, function(req, res, next) {
-  var requestPath = req.params[0].replace(/^\//,'')
-  if (requestPath === '') requestPath = config.defaultGist
-  //varify requestPath against a regex
+function renderGist(id, res) {
   res.write('<script>function copy(from,to){document.querySelector(to).innerHTML=document.querySelector(from).innerHTML}</script><body><div id="content">loading...</div></body>')
-  github.gists.get({ id: requestPath }, function(error, gist) {
+  getGist(id, function(error, gist) {
     if (error) return res.end('error!')
     var files = Object.keys(gist.files).map(function(filename){return gist.files[filename]})
     var mdFiles = files.filter(function(file){return file.language === 'Markdown'})
     var html = mdFiles.map(function(file){return marked(file.content)}).join('<hr />')
     res.end('<span id="source" style="display:none">' + html + '</span><script>copy("#source","#content")</script>')
   })
+}
+
+var app = express()
+app.use(express.static(__dirname + '/public/'))
+app.get(/^(.*)$/, function(req, res, next) {
+  var requestPath = req.params[0].replace(/^\//,'') || config.defaultGist
+  if (validGistIdRegex.test(requestPath)) renderGist(requestPath, res)
+  else res.send('not a valid gist id')
 })
 
 app.use(function(err, req, res, next){
   console.error(err.stack)
-  res.status(500).send('Something broke!')
+  res.status(500).end('Something broke!')
 })
 
 app.listen(config.port)
